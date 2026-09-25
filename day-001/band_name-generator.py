1ef421bbe83973104bd5b201f5d19dd28e7f8ab1
Casey Jones <cjone345@wgu.edu>
2026-09-25 13:42:59 -0400

    Initial 100 Days of Python setup

---
+++ day-001/band_name-generator.py
@@ -0,0 +1,11 @@
+# AUTHOR: Casey Jones
+# CREATED: 9/25/2026
+# PROJECT: Udemy - 100 Days of Python - Day 001 - Band Name Generator
+
+# This program takes user input and stores them as variables, color and creature.
+# Those variable inputs are then used to generate the user's Band Name.
+
+print("Hello and welcome to the Band Name Generator!\n")
+color = input("What is your favorite color?\n")
+creature = input("What is your favorite mythical creature?\n")
+print("Your band name is, The " + color + " " + creature + "'s!")
